6fc41ddddcdc69803d12bd406152e7980ebad061
ajlopez <webmaster@ajlopez.com>
2010-05-26 21:13:53 +0000

    One more test in TransactionTests
    git-svn-id: https://ajcodekatas.googlecode.com/svn/trunk/AjLanguage@223 59a568d8-c386-11dd-8e1c-c7436111cc0c

--- Src/AjLanguage.Tests/TransactionTests.cs
+++ Src/AjLanguage.Tests/TransactionTests.cs
@@ -64,9 +64,30 @@ namespace AjLanguage.Tests
             Assert.AreEqual("bar", reference.GetValue(transaction1));
 
             transaction1.Complete();
+            transaction1.Dispose();
 
             Assert.AreEqual("bar", reference.GetValue());
             Assert.AreEqual("foo", reference.GetValue(transaction2));
+
+            transaction2.Dispose();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void RaiseIfSetValueFromTwoTransactions()
+        {
+            Machine machine = new Machine();
+            TransactionalReference reference = new TransactionalReference();
+            reference.SetValue("foo");
+            Assert.AreEqual("foo", reference.GetValue());
+
+            Transaction transaction1 = new Transaction(machine);
+            Transaction transaction2 = new Transaction(machine);
+
+            reference.SetValue("bar", transaction1);
+            reference.SetValue("newbar", transaction2);
+            transaction1.Complete();
+            transaction2.Complete();
         }
     }
 }
